7436e3e4875b8e7bc7e6e1712bbafad31b8be3df
Akash Kamal <akashkamal.r@gmail.com>
2021-06-10 00:29:38 +0530

    Labels module revamped to side bar (WIP)

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,18 +1,38 @@
 import { useState, useEffect } from 'react';
-import { useHistory,useRouteMatch } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import "../css/HomePage.css"
 import { CgNotes } from "react-icons/cg";
 import { BiLabel } from "react-icons/bi";
 import { BsTrash } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 import { GiGriffinSymbol } from "react-icons/gi";
 import { RiAddLine } from "react-icons/ri";
-import Notes from '../Pages/Notes';
 import NotesEditor from "../Components/NotesEditor"
+import LabelService from "../Services/LabelService"
+
 //import OpenNewEditor from '../Pages/Notes';
 
 export default function SideBar() {
+    const history = useHistory();
     let { path, url } = useRouteMatch();
     const [isPopupOpen, setPopupState] = useState(false)
+    const [labelsList, setLabelsList] = useState([])
+    const [showLabel, setShowLabel] = useState(false)
+
+    useEffect(() => {
+        LabelService.getAllLabels().then(res => {
+            setLabelsList(res)
+            // console.log(res);
+            // if(!params.id){
+            //    params.id = res[0].id;
+            // }
+            // setLabelId(params.id)
+            // setShowNotes(true);
+
+        })
+    }, []);
+
     const sideBarItems = [
         {
             name: "Notes",
@@ -22,7 +42,12 @@ export default function SideBar() {
         {
             name: "Labels",
             route: "/labels",
-            icon: <BiLabel />
+            icon: (showLabel) ? <IoIosArrowUp /> : <IoIosArrowDown/>,
+            subMenu: {
+                subList : labelsList,
+                subMenuIcon :<BiLabel /> ,
+                subMenuStateVariable :  showLabel,
+                route : "/labels/"}
         },
         {
             name: "Trash",
@@ -33,32 +58,46 @@ export default function SideBar() {
 
     const newEditor = () => {
         setPopupState(true)
-       
+
     }
 
-    const history = useHistory();
-    const handleClick = (url, index) => {
-        history.push(url);
+   const handleLabelClick = (url,id) => {
+        history.push(url+id)
+   }
+
+    const handleClick = (url, sublist) => {
+        (sublist) ? setShowLabel(x => !x) : history.push(url)
     }
+
 
     return (
         <>
             <div className="sideBar">
-                <h1 className="row"><GiGriffinSymbol/></h1>
+                <h1 className="row"><GiGriffinSymbol /></h1>
                 <div className="new-note" onClick={() => newEditor()}>
-                                <RiAddLine size={20} />
-                                <span>Add Note</span>
-                            </div>
+                    <RiAddLine size={20} />
+                    <span>Add Note</span>
+                </div>
                 <ul className="sideBarList">
 
                     {
                         sideBarItems.map((item, index) => (
-                            <li key={index} className="row"
-                                id={path.startsWith(item.route) ? "active" : ""}
-                                onClick={() => handleClick(item.route, index)}>
-                                <div id="icon">{item.icon}</div>
-                                <div id="title">{item.name}</div>
-                            </li>
+                            <>
+                                <div key={index} className="row"
+                                    id={path.startsWith(item.route) ? "active" : ""}
+                                    onClick={() => handleClick(item.route, item.subMenu)}>
+                                    <div id="icon">{item.icon}</div>
+                                    <div id="title">{item.name}</div>
+
+                                </div>
+                               
+                                    {(item.subMenu && item.subMenu.subMenuStateVariable) ? item.subMenu.subList.map((subItem, index) => (
+                                        <div  key={index} className="row" onClick={() => handleLabelClick(item.subMenu.route,subItem.id)}>
+                                            <div id="icon">{item.subMenu.subMenuIcon}</div>
+                                    <div id="title">{subItem.labelName}</div></div>
+                                    )) : ""}
+                               
+                            </>
                         )
                         )
                     }
--- src/Pages/Labels.js
+++ src/Pages/Labels.js
@@ -1,32 +1,48 @@
 import React from 'react'
 import Notes from "./Notes"
 import "../css/Labels.css"
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect,useLocation}from 'react';
+import { useHistory } from 'react-router-dom';
 import LabelService from "../Services/LabelService"
 import { IoMdAdd } from "react-icons/io";
 import { BiLabel } from "react-icons/bi";
 import { BiEditAlt } from "react-icons/bi";
 import AddNewLabelPopup from "../Components/AddNewLabelPopup"
+import { useParams } from 'react-router';
 
 function Labels() {
 
-    const [labelList, setLabelsList] = useState([])
-    const [showNotes, setShowNotes] = useState(false)
+    // const [labelList, setLabelsList] = useState([])
+    let params = useParams()
+    // let query = new URLSearchParams(useLocation().search);
+    const [showNotes, setShowNotes] = useState(true)
     const [labelID, setLabelId] = useState()
     const [newLabelPopup,setNewLablePopup] = useState(false)
     const [activeLabel,setActivelabel] = useState();
+  
+    let history = useHistory();
+    
 
     useEffect(() => {
-        LabelService.getAllLabels().then(res => {
-            setLabelsList(res)
-            setLabelId(res[0].id)
-            setShowNotes(true);
-        })
-    }, []);
+        // LabelService.getAllLabels().then(res => {
+        //     setLabelsList(res)
+        //     console.log(res);
+        //     if(!params.id){
+        //        params.id = res[0].id;
+        //     }
+            setLabelId(params.id)
+            setActivelabel(params.id)
+        //     setShowNotes(true);
+            
+        // })
+        console.log(params)
+    }, [params.id]);
 
-    const changelabel = (id) => {
-        setLabelId(id);
-    }
+    // const changelabel = (id) => {
+    //     setLabelId(id);
+    //     history.push("/labels/"+id)
+    //     console.log(params);
+    // }
 
     const editLabel = (label) =>{
         setActivelabel(label);
@@ -37,7 +53,7 @@ function Labels() {
     return (
         <>
             <div className="labels-container">
-                <div className="labels-list">
+                {/* <div className="labels-list">
                     <h3>My labels</h3>
                     <div className="label-list-header">
                         <input type="text" placeholder="Search Labels" className="label-search"></input>
@@ -46,22 +62,22 @@ function Labels() {
                     {
                         labelList.map((value, key) => (
                             <>
-                                <div className="label" onClick={() => changelabel(value.id)} >
+                                <div className={(params.id == value.id) ? "label active":"label" }
+                                id = {(params.id == value.id) ? "active":"" }
+                                onClick={() => changelabel(value.id)} >
                                     <div className="label-icon"><BiLabel size={25} /></div>
                                     <div key={key} className="label-name">{value.labelName}</div>
-                                    <div className="label-edit-icon" onClick={() => {
-                                        console.log("edit label")
-                                    }}><BiEditAlt size={23} onClick={() => editLabel(value)} /></div>
+                                    <div className="label-edit-icon"><BiEditAlt size={23} onClick={() => editLabel(value)} /></div>
 
                                 </div>
                             </>
                         ))
                     }
-                </div>
+                </div> */}
                 <div className="notes-list">
                     {showNotes && <Notes label={labelID} ></Notes>}
                 </div>
-                { newLabelPopup ? <AddNewLabelPopup props={activeLabel} onClose={()=> {setNewLablePopup(false);setActivelabel(null)}}></AddNewLabelPopup> : ""}
+                { params.action ? <AddNewLabelPopup props={params.id} onClose={()=> {history.push("/labels/")}}></AddNewLabelPopup> : ""}
             </div>
            
         </>
